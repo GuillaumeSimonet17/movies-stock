352858c0a2cd6807d7e4446a1d28f1956f01bd3b
Guillaume Simonet <guillaume@krafter.io>
2024-06-30 00:46:12 +0200

    next and previous button in movie page, and redirect home onclick header title

--- moviesstock/static/msapp/js/go_to.js
+++ moviesstock/static/msapp/js/go_to.js
@@ -7,7 +7,7 @@ function goToMoviePage(id) {
 }
 
 function goToNextMoviePage(movies_list, id) {
-     let nextId = 0;
+     let nextId = -1;
 
      for (let i = 0; i < movies_list.length; i++) {
           if (movies_list[i] == id) {
@@ -17,7 +17,22 @@ function goToNextMoviePage(movies_list, id) {
                break;
           }
      }
-     if (nextId === 0)
+     if (nextId === -1)
           nextId = movies_list[0]
      window.location.href = '/movie_page/?query=' + nextId;
+}
+
+function goToPreviousMoviePage(movies_list, id) {
+     let previousId = -1;
+
+     for (let i = 0; i < movies_list.length; i++) {
+          if (movies_list[i] !== undefined && movies_list[i] == id) {
+               previousId = movies_list[i - 1];
+               break;
+          }
+     }
+
+     if (previousId === undefined)
+          previousId = movies_list[movies_list.length-1]
+     window.location.href = '/movie_page/?query=' + previousId;
 }
