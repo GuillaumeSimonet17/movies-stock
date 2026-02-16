610c136f8f64f46ef52f467de6946a2563a728dd
guillaume <guillaume.simonet.info@gmail.com>
2026-02-16 12:46:08 +0100

    [FIX] Fix Vite config & settings.py.

--- backend/moviesstock/settings.py
+++ backend/moviesstock/settings.py
@@ -121,9 +121,16 @@ CORS_ALLOW_CREDENTIALS = True
 
 # CSRF Configuration
 CSRF_TRUSTED_ORIGINS = [
+    'https://hollylist.guillaume-simonet.com',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
 ]
+
+SESSION_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SECURE = True
 
 # REST Framework Configuration
 REST_FRAMEWORK = {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    hmr: false,
     allowedHosts: [
       "hollylist.guillaume-simonet.com"
     ],
