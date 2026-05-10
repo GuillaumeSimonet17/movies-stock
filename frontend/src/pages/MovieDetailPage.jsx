e75c8b8bde68271aa8ee2bbf16804a3d7746be59
guillaume <guillaume.simonet.info@gmail.com>
2026-05-10 13:50:08 +0200

    [FIX] Delete movie.

--- frontend/src/pages/MovieDetailPage.jsx
+++ frontend/src/pages/MovieDetailPage.jsx
@@ -87,7 +87,7 @@ function MovieDetailPage() {
       await movieService.deleteMovie(id);
       navigate('/');
     } catch {
-      alert('Failed to delete movie');
+      print('Failed to delete movie');
     }
   };
 
